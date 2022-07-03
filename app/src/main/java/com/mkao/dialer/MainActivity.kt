2a43b6d2644915254ecef3eee804558f4decba6e
mkao <mkaoibrahim@gmail.com>
2022-07-03 22:34:39 +0300

    Initial commit

---
+++ app/src/main/java/com/mkao/dialer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mkao.dialer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
