60fae9cbd18688fb2d10219e496eee402bfc2e90
mkao <mkaoibrahim@gmail.com>
2022-07-09 10:31:34 +0300

    Dial Functionality Updated

--- app/src/main/java/com/mkao/dialer/MainActivity.kt
+++ app/src/main/java/com/mkao/dialer/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.mkao.dialer
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,7 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun callNumber(toString: String) {
-
+    fun callNumber(number: String) {
+        // if the permissions not granted prompt
+        //packages the data and converts phone number in URI Intent
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.CALL_PHONE)==PackageManager.PERMISSION_GRANTED)
+        {
+            val  intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$number")
+            startActivity(intent)
+        }else ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE),0)
     }
 }
