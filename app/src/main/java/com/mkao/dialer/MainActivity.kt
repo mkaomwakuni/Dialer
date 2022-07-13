873a0715364ebd3da17199f0f05afdccadf925fc
mkao <mkaoibrahim@gmail.com>
2022-07-13 23:50:21 +0300

    Read Permissions Updated for CallLogs

--- app/src/main/java/com/mkao/dialer/MainActivity.kt
+++ app/src/main/java/com/mkao/dialer/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.activity.viewModels
@@ -13,9 +14,26 @@ import java.util.jar.Manifest
 class MainActivity : AppCompatActivity() {
     private val communicationViewModel:CommunicationViewModel by viewModels()
 
+    companion object{
+        //Reference code allows the MainActivity keep track and respond to permission requests
+        const val READ_STORAGE_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode){
+            READ_STORAGE_REQUEST_CODE-> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) getCallLogs()
+        }
     }
 
     fun callNumber(number: String) {
@@ -36,6 +54,16 @@ class MainActivity : AppCompatActivity() {
         if (readStoragePermission!=PackageManager.PERMISSION_GRANTED || readCallLogsPermission != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,android.Manifest.permission.READ_CALL_LOG),READ_STORAGE_REQUEST_CODE)
             return
+        }
+        val cursor = application.contentResolver.query(CallLog.Calls.CONTENT_URI,null,null,null,CallLog.Calls.DATE + "DESC")
+        val callLog = mutableListOf<CallLogEvent>()
+        cursor?.use {
+            val number = it.getColumnIndexOrThrow(CallLog.Calls.NUMBER)
+            val type = it.getColumnIndexOrThrow(CallLog.Calls.TYPE)
+            val date = it.getColumnIndexOrThrow(CallLog.Calls.DATE)
+            while (it.moveToNext()){
+                val phoneNumber = cursor.getString(number)
+            }
         }
     }
 
