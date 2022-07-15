7150e58acddbb9fc24fcd9430e72d065ff9dc5e5
mkao <mkaoibrahim@gmail.com>
2022-07-15 18:45:15 +0300

    Displaying SMS Objects

--- app/src/main/java/com/mkao/dialer/CommunicationViewModel.kt
+++ app/src/main/java/com/mkao/dialer/CommunicationViewModel.kt
@@ -2,7 +2,9 @@ package com.mkao.dialer
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mkao.dialer.ui.sms.SMS
 
 class CommunicationViewModel:ViewModel(){
     var callLog = MutableLiveData<List<CallLogEvent>>()
+    val texts = MutableLiveData<List<SMS>>()
 }
---
+++ app/src/main/java/com/mkao/dialer/ui/sms/SMS.kt
@@ -0,0 +1,3 @@
+package com.mkao.dialer.ui.sms
+
+ data class SMS(val sender: String,val body:String )
